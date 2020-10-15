d1409bd78c91564cc07a397edc91a471eec9e87d
Yasir Hussain <48673014+hussainyasir23@users.noreply.github.com>
2020-10-15 12:40:15 +0530

    Create FlipStringtoMonotoneIncreasing.cpp

---
+++ FlipStringtoMonotoneIncreasing.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minFlipsMonoIncr(string S) {
+        int N = S.length();
+        int P[N+1];
+        for (int i = 0; i < N; ++i)
+            P[i+1] = P[i] + (S[i] == '1' ? 1 : 0);
+        int ans = INT_MAX;
+        for (int j = 0; j <= N; ++j) 
+            ans = min(ans, P[j] + N-j-(P[N]-P[j]));
+        return ans;
+    }
+};
+/*Basically we go through string and found out how much 1 before index much be flipped to 0 and how much 0 after index need to be flipped to 1. adds them up and get min for result*/
